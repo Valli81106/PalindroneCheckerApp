e86da306aef9b2a3df21fca4781e8fb59bcf531b
Valli81106 <sreevalliiscute1234@gmail.com>
2026-02-25 09:36:44 +0530

    UC1: Mera hai application entry and hi

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,6 +2,7 @@ public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Halloooo, welcome to the Plaindrome checker app");
         System.out.println("System stabalized successfully");
+        System.out.println("This is from sreevalli");
     }
 
 
