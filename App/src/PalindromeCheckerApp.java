60a0361e22c73d8c498ebddf2d3a6a886645e27a
vaibhav.vasistha06@gmail.com <saiuppalapati16@gmail.com>
2026-02-25 09:10:47 +0530

    UC1 : Welcome mesg

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Halloooo, welcome to the Plaindrome checker app");
+        System.out.println("System stabalized successfully");
+    }
+
+
+}
